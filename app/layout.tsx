19f847a171485300f9f24a76f9ccfc4fa7c602af
Jose Tovar <jtovar26455462@gmail.com>
2026-01-29 17:21:01 -0400

    config: add project main config, ande dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,31 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'portfolio',
+  title: 'Portafolio | Jose Tovar',
   description: 'Jose Tovar portfolio project',
 }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
-      <body>{children}</body>
+    <html lang="es" className="scroll-smooth" suppressHydrationWarning>
+      <body className={`${inter.className} bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
